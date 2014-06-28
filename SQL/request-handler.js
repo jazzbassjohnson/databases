d5f1a7cc8b6a2a6f53fed31b33aba1e68e705892
Hack Reactor Students <garydotjs@gmail.com>
2014-06-27 19:59:14 -0700

    Working on SQL Tables

---
+++ SQL/request-handler.js
@@ -0,0 +1,76 @@
+/* You should implement your request handler function in this file.
+ * And hey! This is already getting passed to http.createServer()
+ * in basic-server.js. But it won't work as is.
+ * You'll have to figure out a way to export this function from
+ * this file and include it in basic-server.js so that it actually works.
+ * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var fs = require('fs');
+var exports = module.exports = {};
+var body = {};
+body.results = [];
+exports.handleRequest = function(request, response) {
+  /* the 'request' argument comes from nodes http module. It includes info about the
+  request - such as what URL the browser is requesting. */
+
+  /* Documentation for both request and response can be found at
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var statusCode = 200;
+
+  /* Without this line, this server wouldn't work. See the note
+   * below about CORS. */
+  var headers = exports.defaultCorsHeaders;
+
+  headers['Content-Type'] = "application/json";
+  /* .writeHead() tells our server what HTTP status code to send back */
+  // response.writeHead(statusCode, headers);
+  if((request.url === '/classes/messages' || request.url === '/classes/room1') && request.method === 'GET'){
+    response.writeHead(statusCode, headers);
+
+    //********************** connect to database  - retrieve right data
+
+
+
+    response.end(JSON.stringify(body));
+  }
+ else if((request.url === '/classes/messages' || request.url === '/classes/room1') && request.method === 'POST'){
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    request.on('data', function(data){
+      if(data !== undefined) {
+
+
+      //********************** connect to database  - update its data
+
+
+      body.results.push(JSON.parse(data));
+      }
+
+
+
+    });
+    response.end(JSON.stringify(body));
+  }else{
+
+
+
+    statusCode = 404;
+    response.writeHead(statusCode, headers);
+    response.end();
+  }
+
+};
+
+/* These headers will allow Cross-Origin Resource Sharing (CORS).
+ * This CRUCIAL code allows this server to talk to websites that
+ * are on different domains. (Your chat client is running from a url
+ * like file://your/chat/client/index.html, which is considered a
+ * different domain.) */
+exports.defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
--- SQL/schema.sql
+++ SQL/schema.sql
@@ -2,19 +2,47 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `rooms`
+CREATE TABLE `rooms`
+(
+  `room_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `room_name` varchar(20)
 );
 
+
 /* Create other tables and define schemas for them here! */
-
-
-
+DROP TABLE IF EXISTS 'users'
+CREATE TABLE `users`
+(
+  `user_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `user_name` varChar(10),
+  `user_password` varChar(10),
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
 
+DROP TABLE IF EXISTS `messages`
+CREATE TABLE messages
+(
+  `message_id` INT AUTO_INCREMENT PRIMARY KEY,
+  `message_text` varChar(50),
+  `user_id` INT,
+  `room_id` INT,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
 
 
 
+
+
+
+
+
+
+-- INSERT INTO supportContacts
+-- (roomName)
+-- VALUES
+-- ('Gar&Cel');
